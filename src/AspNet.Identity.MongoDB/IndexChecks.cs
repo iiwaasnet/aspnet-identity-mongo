4a9b41d2f3b7ed02e35af04de8a05466f4a75f29
Richard <chen4901@hotmail.com>
2015-03-22 15:21:45 -0700

    use MongoDb c# driver from beta4 to rc0

--- src/AspNet.Identity.MongoDB/IndexChecks.cs
+++ src/AspNet.Identity.MongoDB/IndexChecks.cs
@@ -8,21 +8,21 @@
         public static Task EnsureUniqueIndexOnUserName<TUser>(this IMongoCollection<TUser> users)
             where TUser : IdentityUser
         {
-            return users.Indexes.CreateAsync(new IndexDefinitionBuilder<TUser>().Ascending(u => u.UserName),
+            return users.Indexes.CreateOneAsync(new IndexKeysDefinitionBuilder<TUser>().Ascending(u => u.UserName),
                                              new CreateIndexOptions {Background = true, Unique = true});
         }
 
         public static Task EnsureUniqueIndexOnEmail<TUser>(this IMongoCollection<TUser> users)
             where TUser : IdentityUser
         {
-            return users.Indexes.CreateAsync(new IndexDefinitionBuilder<TUser>().Ascending(u => u.Email),
+            return users.Indexes.CreateOneAsync(new IndexKeysDefinitionBuilder<TUser>().Ascending(u => u.Email),
                                              new CreateIndexOptions {Background = true, Unique = true});
         }
 
         public static Task EnsureUniqueIndexOnRoleName<TRole>(this IMongoCollection<TRole> roles)
             where TRole : IdentityRole
         {
-            return roles.Indexes.CreateAsync(new IndexDefinitionBuilder<TRole>().Ascending(r => r.Name),
+            return roles.Indexes.CreateOneAsync(new IndexKeysDefinitionBuilder<TRole>().Ascending(r => r.Name),
                                              new CreateIndexOptions {Background = true, Unique = true});
         }
     }
